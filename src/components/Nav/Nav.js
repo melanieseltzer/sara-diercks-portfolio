b63b113f4a0fc3edbdda6baf9f48e19ba6b0868a
Melanie Seltzer <melleh11@gmail.com>
2018-12-04 19:07:14 -0800

    Fix jsx-ally error

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,17 +2,28 @@
 import React from 'react';
 import Link from 'next/link';
 
+// NOTE: The Next.js Link API forces you to add an <a> inside <Link>
+// which then gets its href passed down. This triggers `eslint-plugin-jsx-a11y`
+// to throw an error due to the `noHref` rule. It's been disabled temporarily
+// until a solution is found (issue: https://github.com/zeit/next.js/issues/5533)
+
 export default () => (
   <nav>
     <ul>
       <li>
-        <Link href="#about">About</Link>
+        <Link href="#about">
+          <a>About</a>
+        </Link>
       </li>
       <li>
-        <Link href="#work">Work</Link>
+        <Link href="#work">
+          <a>Work</a>
+        </Link>
       </li>
       <li>
-        <Link href="#contact">Contact</Link>
+        <Link href="#contact">
+          <a>Contact</a>
+        </Link>
       </li>
     </ul>
   </nav>
