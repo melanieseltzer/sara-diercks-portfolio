a8c9d93b5bcc7dbb301649dfcb5bbf11a105fae7
Melanie Seltzer <melleh11@gmail.com>
2018-12-10 14:36:07 -0800

    DRY nav render

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,30 +3,24 @@ import React from 'react';
 import Link from 'next/link';
 import styled from 'styled-components';
 
+const pages = ['About', 'Work', 'Contact'];
+
+// Render a single nav item in the UI
 // NOTE: The Next.js Link API forces you to add an <a> inside <Link>
 // which then gets its href passed down. This triggers `eslint-plugin-jsx-a11y`
-// to throw an error due to the `noHref` rule. It's been disabled temporarily
+// to throw an error due to the `noHref` rule. The rule has been disabled temporarily
 // until a solution is found (issue: https://github.com/zeit/next.js/issues/5533)
+const navItem = name => (
+  <li>
+    <Link href={`#${name.toLowerCase()}`} passHref>
+      <StyledLink>{name}</StyledLink>
+    </Link>
+  </li>
+);
 
 export default () => (
   <Nav>
-    <ul>
-      <li>
-        <Link href="#about" passHref>
-          <StyledLink>About</StyledLink>
-        </Link>
-      </li>
-      <li>
-        <Link href="#work" passHref>
-          <StyledLink>Work</StyledLink>
-        </Link>
-      </li>
-      <li>
-        <Link href="#contact" passHref>
-          <StyledLink>Contact</StyledLink>
-        </Link>
-      </li>
-    </ul>
+    <ul>{pages.map(page => navItem(page))}</ul>
   </Nav>
 );
 
