bd03d6c69d1a8f1a8e1609677f1e063be2ea6615
Melanie Seltzer <melleh11@gmail.com>
2018-12-24 21:49:51 -0800

    Connect to GraphCMS (#17)

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export const GRAPHQL_URL =
+  'https://api-uswest.graphcms.com/v1/cjons8vsw2c5t01g5jv8lyn7y/master';
---
+++ backend/withApollo.js
@@ -0,0 +1,12 @@
+// @flow
+import withApollo from 'next-with-apollo';
+import ApolloClient, { InMemoryCache } from 'apollo-boost';
+import { GRAPHQL_URL } from './config';
+
+export default withApollo(
+  ({ initialState }) =>
+    new ApolloClient({
+      uri: GRAPHQL_URL,
+      cache: new InMemoryCache().restore(initialState || {})
+    })
+);
