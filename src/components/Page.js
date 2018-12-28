19746409c8a6565caf734c9d3fd338334fa98bdc
Melanie Seltzer <melleh11@gmail.com>
2018-12-28 14:17:17 -0800

    Add favicon

--- src/components/Page.js
+++ src/components/Page.js
@@ -18,6 +18,7 @@ export default ({
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>{pageTitle} | Portfolio of Sara Diercks</title>
       <meta name="description" content={metaDescription} />
+      <link rel="icon" type="image/x-icon" href="../static/favicon.png" />
       <link
         href="https://fonts.googleapis.com/css?family=Roboto:300,400"
         rel="stylesheet"
