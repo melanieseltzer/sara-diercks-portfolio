f7e7ceccc3d56a63185af6de73d662b8b725a73f
Melanie Seltzer <melleh11@gmail.com>
2019-01-01 20:31:23 -0800

    Fix flow errors

--- src/fonts.js
+++ src/fonts.js
@@ -1,22 +1,29 @@
+// @flow
 const FontFaceObserver = require('fontfaceobserver');
 
-const LoadFonts = () => {
+const LoadFonts = (): void => {
   const link = document.createElement('link');
   link.href =
     'https://fonts.googleapis.com/css?family=Lora:400i|Roboto:300,400';
   link.rel = 'stylesheet';
 
-  document.head.appendChild(link);
+  if (document.head) {
+    document.head.appendChild(link);
+  }
 
   const Roboto = new FontFaceObserver('Roboto');
   const Lora = new FontFaceObserver('Lora');
 
   Promise.all([Roboto.load(), Lora.load()])
     .then(() => {
-      document.documentElement.classList.add('fonts-loaded');
+      if (document.documentElement) {
+        document.documentElement.classList.add('fonts-loaded');
+      }
     })
     .catch(() => {
-      document.documentElement.classList.add('fonts-failed');
+      if (document.documentElement) {
+        document.documentElement.classList.add('fonts-failed');
+      }
     });
 };
 
