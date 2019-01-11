ca85a2a8ce10ca3feac97f592bc8b4fd8c3208c1
Melanie Seltzer <melleh11@gmail.com>
2019-01-11 14:24:36 -0800

    Try switch to Now v2 (#30)

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withPlugins = require('next-compose-plugins');
 const withCSS = require('@zeit/next-css');
 
 const nextConfig = {
-  distDir: 'build'
+  target: 'serverless'
 };
 
 module.exports = withPlugins([[withCSS]], nextConfig);
