3d78361f41c790b36f1b56cd045ff2fe1afcd843
Melanie Seltzer <melleh11@gmail.com>
2019-01-20 11:56:24 -0800

    Tweak SEO and redirect

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,13 @@ import React from 'react';
 import About from '../src/components/About';
 import Contact from '../src/components/Contact';
 import Intro from '../src/components/Intro';
-import Page from '../src/components/Page';
+import Page from '../src/layout/Page';
 import Projects from '../src/components/Projects';
 
 const Index = () => (
   <Page
-    pageTitle="Home"
-    metaDescription="Showcasing projects from UCLA Extension's GIS and Geospatial Technology certificate program."
+    pageTitle="GIS Projects"
+    metaDescription="Portfolio of Sara Diercks, showcasing projects from UCLA Extension's GIS and Geospatial Technology certificate program."
   >
     <Intro />
     <Projects />
--- src/layout/Page.js
+++ src/layout/Page.js
@@ -18,6 +18,7 @@ const Page = ({
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>{pageTitle} | Portfolio of Sara Diercks</title>
       <meta name="description" content={metaDescription} />
+      <link rel="canonical" href="https://saradiercks.com" />
       <link rel="icon" type="image/x-icon" href="../static/favicon.ico" />
     </Head>
     <main>{children}</main>
